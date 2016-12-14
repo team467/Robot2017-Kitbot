a64929bbd10bbdc5b4aaf39339b5fa16a36b1535
Nathan Petrangelo <nathan@petrangelo.org>
2016-12-13 19:55:25 -0500

    First Commit with Code

---
+++ src/org/usfirst/frc/team467/robot/Robot.java
@@ -0,0 +1,52 @@
+package org.usfirst.frc.team467.robot;
+
+
+import edu.wpi.first.wpilibj.SampleRobot;
+import edu.wpi.first.wpilibj.RobotDrive;
+import edu.wpi.first.wpilibj.Joystick;
+import edu.wpi.first.wpilibj.Timer;
+
+/**
+ * This is a demo program showing the use of the RobotDrive class, specifically it 
+ * contains the code necessary to operate a robot with tank drive.
+ *
+ * The VM is configured to automatically run this class, and to call the
+ * functions corresponding to each mode, as described in the SampleRobot
+ * documentation. If you change the name of this class or the package after
+ * creating this project, you must also update the manifest file in the resource
+ * directory.
+ *
+ * WARNING: While it may look like a good choice to use for your code if you're inexperienced,
+ * don't. Unless you know what you are doing, complex code will be much more difficult under
+ * this system. Use IterativeRobot or Command-Based instead if you're new.
+ */
+public class Robot extends SampleRobot {
+    RobotDrive frontWheels;  // class that handles basic drive operations
+    RobotDrive backWheels;  // class that handles basic drive operations
+
+    Joystick leftStick;  // set to ID 1 in DriverStation
+    Joystick rightStick; // set to ID 2 in DriverStation
+    public Robot() {
+        frontWheels = new RobotDrive(6, 8);
+        frontWheels.setExpiration(0.1);
+        backWheels = new RobotDrive(7, 9);
+        backWheels.setExpiration(0.1);
+        leftStick = new Joystick(0);
+        rightStick = new Joystick(1);
+    }
+
+    
+    /**
+     * Runs the motors with tank steering.
+     */
+    public void operatorControl() {
+        frontWheels.setSafetyEnabled(true);
+        backWheels.setSafetyEnabled(true);
+        while (isOperatorControl() && isEnabled()) {
+        	frontWheels.setLeftRightMotorOutputs(-leftStick.getY(), -rightStick.getY());
+        	backWheels.setLeftRightMotorOutputs(-leftStick.getY(), -rightStick.getY());
+            Timer.delay(0.005);		// wait for a motor update time
+        }
+    }
+
+}
